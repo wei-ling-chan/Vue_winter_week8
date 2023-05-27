eac62ad0dcca5891c9a9c304e145dd54ea420a6c
您的姓名 <Email>
2023-05-28 03:15:36 +0800

    modify mystorage

--- src/views/front/myStorage.vue
+++ src/views/front/myStorage.vue
@@ -1,38 +1,38 @@
 <template>
   <div style="height: 20vh;"></div>
   <div class="filter-product my-md-5 my-4 tasty-list" ref="filter-product">
-                <!-- <div class="d-flex justify-content-center">
-                </div> -->
-                <div class="container">
-                  <h1 class="text-center mb-md-4 m-0">我的收藏</h1>
-                  <div class="row" >
-                    <div class="col-12 col-md-6 col-xl-4 tasty-item-block" v-for="product in temProducts" :key="product.id">
-                      <div class="card mx-2 my-5">
-                        <!-- <img :src="product.imageUrl" alt="" class="slide-img"> -->
-                        <div class="slide-hover">
-                          <img :src="product.imageUrl" alt="" class="slide-img">
-                        </div>
-                        <div class="card-body p-3">
-                        <div class="mb-4 tasty-item">
-                          <div class="d-flex justify-content-between align-items-center mb-2">
-                            <h5 class="card-title fw-bolder tasty-item-title d-block">{{product.title}}</h5>
-                            <div><i :class="heartClass(product.id)" @click="toggleFavorite(product.id)"></i></div>
-                          </div>
-                          <div class="d-flex justify-content-between">
-                            <div class="card-text  text-md end tasty-item-price fs-7">原價 <span class="text-decoration-line-through"> NT$ {{product.origin_price}}</span></div>
-                            <div class="card-text  text-md end tasty-item-price text-danger">優惠價 NT$ {{product.price}}</div>
-                          </div>
-                        </div>
-                        <div class="d-flex justify-content-between mb-1  tasty-button">
-                          <router-link :to="`/product/${product.id}`" class="d-md-block btn btn-info text-light more py-2">查看更多</router-link>
-                          <button type="button" class="d-md-block btn btn-info text-light" @click="addToCart(product.id,qty)">加入購物車</button>
-                        </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+    <div class="container">
+      <h1 class="text-center mb-md-4 m-0">我的收藏</h1>
+      <div class="row" v-if="temProducts.length>0">
+        <div class="col-12 col-md-6 col-xl-4 tasty-item-block" v-for="product in temProducts" :key="product.id">
+          <div class="card mx-2 my-5">
+            <div class="slide-hover">
+              <img :src="product.imageUrl" alt="" class="slide-img">
+            </div>
+            <div class="card-body p-3">
+            <div class="mb-4 tasty-item">
+              <div class="d-flex justify-content-between align-items-center mb-2">
+                <h5 class="card-title fw-bolder tasty-item-title d-block">{{product.title}}</h5>
+                <div><i :class="heartClass(product.id)" @click="toggleFavorite(product.id)"></i></div>
               </div>
+              <div class="d-flex justify-content-between">
+                <div class="card-text  text-md end tasty-item-price fs-7">原價 <span class="text-decoration-line-through"> NT$ {{product.origin_price}}</span></div>
+                <div class="card-text  text-md end tasty-item-price text-danger">優惠價 NT$ {{product.price}}</div>
+              </div>
+            </div>
+            <div class="d-flex justify-content-between mb-1  tasty-button">
+              <router-link :to="`/product/${product.id}`" class="d-md-block btn btn-info text-light more py-2">查看更多</router-link>
+              <button type="button" class="d-md-block btn btn-info text-light" @click="addToCart(product.id,qty)">加入購物車</button>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <div class="h5 py-7 text-center">目前還沒有收藏喔!趕快去收藏吧~</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -50,12 +50,11 @@ export default {
   methods: {
     testItems () {
       this.favorites = JSON.parse(localStorage.getItem('favorites')) || []
-      // console.log(this.favorites)
       this.$http.get(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/products/all`)
         .then((res) => {
           this.products = res.data.products
-          console.log(this.favorites)
           this.filteredItems()
+          console.log(this.products, this.temProducts, this.favorites)
         })
     },
     filteredItems () {
