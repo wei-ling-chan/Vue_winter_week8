fa0400b662823d9725ae6683700e29f308aa3bd5
wlc <wlc606@gmail.com>
2023-06-08 02:07:39 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 
-/* global process */
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,5 +10,5 @@ export default defineConfig({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
     })
   ],
-  base: process.env.NODE_ENV === 'production' ? '/vue_winter_week8/' : '/'
+  base: '/vue_winter_week8'
 })
